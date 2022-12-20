7f153b2df89fbd0633682edc5959b12c4d9519eb
Randwarf <matas.vaitkevicius@gmail.com>
2022-12-20 19:41:49 +0200

    Patvirtinus, pridedamas admin štampas prie prekės

--- administravimas/atmesti.php
+++ administravimas/atmesti.php
@@ -1,7 +1,7 @@
 <?php
 
 include($_SERVER['DOCUMENT_ROOT']."/isp_aukcionai/include/header.php");
-$query = "UPDATE aukcionas SET statusas=2 WHERE fk_Prekeid_Preke=" . $_GET['id'];
+$query = "UPDATE aukcionas SET statusas=2, fk_Administratoriusid_Administratorius=".$USERINFO['fk_Administratorius']." WHERE fk_Prekeid_Preke=" . $_GET['id'];
 mysqli_query($db, $query);
 header("Location: nepatvirtintos.php");
 
--- administravimas/patvirtinti.php
+++ administravimas/patvirtinti.php
@@ -1,7 +1,8 @@
 <?php
 
 include($_SERVER['DOCUMENT_ROOT']."/isp_aukcionai/include/header.php");
-$query = "UPDATE aukcionas SET statusas=1 WHERE fk_Prekeid_Preke=" . $_GET['id'];
+$query = "UPDATE aukcionas SET statusas=1, fk_Administratoriusid_Administratorius=".$USERINFO['fk_Administratorius']." WHERE fk_Prekeid_Preke=" . $_GET['id'];
+echo $query;
 mysqli_query($db, $query);
 header("Location: nepatvirtintos.php");
 
